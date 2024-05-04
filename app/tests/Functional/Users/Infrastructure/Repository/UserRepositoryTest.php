fd22c0272c5a9f5f4f5f46e92bfca69f63af63d1
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2024-05-04 12:57:32 +0500

    feat. fixture created

--- app/tests/Functional/Users/Infrastructure/Repository/UserRepositoryTest.php
+++ app/tests/Functional/Users/Infrastructure/Repository/UserRepositoryTest.php
@@ -2,10 +2,14 @@
 
 namespace App\Tests\Functional\Users\Infrastructure\Repository;
 
+use App\Tests\Resource\Fixtures\UserFixture;
+use App\Users\Domain\Entity\User;
 use App\Users\Domain\Factory\UserFactory;
 use App\Users\Infrastructure\Repository\UserRepository;
 use Faker\Factory;
 use Faker\Generator;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserRepositoryTest extends WebTestCase
@@ -15,25 +19,42 @@ class UserRepositoryTest extends WebTestCase
      */
     private UserRepository $repository;
     private Generator $faker;
+    private AbstractDatabaseTool $databaseTool;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->repository = static::getContainer()->get(UserRepository::class);
         $this->faker = Factory::create();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
     }
 
     public function test_user_added_successfully(): void
     {
+        // arrange
         $email = $this->faker->email();
         $password = $this->faker->password();
         $user = (new UserFactory())->create($email, $password);
 
-//        act
+        // act
         $this->repository->add($user);
 
-//        assert
+        // assert
         $existedUser = $this->repository->getByUlid($user->getUlid());
+        $this->assertEquals($user->getUlid(), $existedUser->getUlid());
+    }
+
+    public function test_user_found_successfully(): void
+    {
+        // arrange
+        $executor = $this->databaseTool->loadFixtures([UserFixture::class]);
+        /** @var User $user */
+        $user = $executor->getReferenceRepository()->getReference(UserFixture::REFERENCE);
+
+        // act
+        $existedUser = $this->repository->getByUlid($user->getUlid());
+
+        // assert
         $this->assertEquals($user->getUlid(), $existedUser->getUlid());
     }
 }
